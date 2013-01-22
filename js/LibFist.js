930544e72247daef1b0a625d5637a6cc5b55e6b9
Evan Savage <savage.evan@gmail.com>
2013-01-22 12:14:32 -0800

    Updating describe() blurbs in LibFist
    to take advantage of the new documentation system.

--- js/LibFist.js
+++ js/LibFist.js
@@ -77,9 +77,9 @@ var OpsArith = {
   add: new FistFunction(function(args) {
     var channels = [],
         numberSum = 0;
-    for (var i = 0; i < args.xs.length; i++) {
-      var arg = args.xs[i],
-          argType = fist.evaluateType(args.__sexps.xs[i]);
+    for (var i = 0; i < args.values.length; i++) {
+      var arg = args.values[i],
+          argType = fist.evaluateType(args.__sexps.values[i]);
       if (argType === 'number') {
         numberSum += arg;
       } else {
@@ -101,14 +101,14 @@ var OpsArith = {
         return UnionIterator(channels.map(function(c) { return c.iter(); }));
       }
     };
-  }).type('(fn (name (+ channel?) "xs") (max (ref "xs")))')
-    .describe('Takes the sum of its parameters.'),
+  }).type('(fn (name (+ channel?) "values") (max (ref "values")))')
+    .describe('Takes the sum of its values.'),
   multiply: new FistFunction(function(args) {
     var channels = [],
         numberProd = 1;
-    for (var i = 0; i < args.xs.length; i++) {
-      var arg = args.xs[i],
-          argType = fist.evaluateType(args.__sexps.xs[i]);
+    for (var i = 0; i < args.values.length; i++) {
+      var arg = args.values[i],
+          argType = fist.evaluateType(args.__sexps.values[i]);
       if (argType === 'number') {
         numberProd *= arg;
       } else {
@@ -130,8 +130,8 @@ var OpsArith = {
         return UnionIterator(channels.map(function(c) { return c.iter(); }));
       }
     };
-  }).type('(fn (name (+ channel?) "xs") (max (ref "xs")))')
-    .describe('Takes the product of its parameters.'),
+  }).type('(fn (name (+ channel?) "values") (max (ref "values")))')
+    .describe('Takes the product of its values.'),
   subtract: new FistFunction(function(args) {
     if (args.b === undefined) {
       return _unaryOp(args.a, function(a) {
@@ -143,24 +143,24 @@ var OpsArith = {
     });
   }).type('(fn (-> (name channel? "a") (name (? channel?) "b")) (max (ref "a") (ref "b")))')
     .describe(
-      'With one parameter, negates that parameter. ' +
-      'With two parameters, subtracts the second parameter from the first.'
+      'If only a is provided, negates a. ' +
+      'If (a, b) are both provided, produces the difference a - b.'
     ),
   divideFloat: new FistFunction(function(args) {
     return _binaryOp(args.a, args.b, function(a, b) {
       return a / b;
     })
   }).type('(fn (-> (name channel? "a") (name channel? "b")) (max (ref "a") (ref "b")))')
-    .describe('Divides the first parameter by the second.'),
+    .describe(
+      'Produces the quotient a / b including any fractional part.'
+    ),
   divideInt: new FistFunction(function(args) {
     return _binaryOp(args.a, args.b, function(a, b) {
       return Math.floor(a / b);
     });
   }).type('(fn (-> (name channel? "a") (name channel? "b")) (max (ref "a") (ref "b")))')
     .describe(
-      'Divides the first parameter by the second. ' +
-      'The resulting number or channel values are rounded down ' +
-      'to the nearest integer.'
+      'Produces the quotient a / b rounded down to the nearest integer.'
     ),
   mod: new FistFunction(function(args) {
     return _binaryOp(args.a, args.b, function(a, b) {
@@ -168,7 +168,8 @@ var OpsArith = {
     });
   }).type('(fn (-> (name channel? "a") (name number "b")) (max (ref "a") (ref "b")))')
     .describe(
-      'Computes the first parameter modulo the second.'
+      'Produces the modulus a % b, or the remainder left over from ' +
+      'the quotient a / b. For instance, (% 8 5) is 3.'
     ),
   bucket: new FistFunction(function(args) {
     return _binaryOp(args.a, args.b, function(a, b) {
@@ -176,9 +177,9 @@ var OpsArith = {
     });
   }).type('(fn (-> (name channel? "a") (name number "b")) (ref "a"))')
     .describe(
-      'Computes the highest integer multiple of the second parameter less ' +
-      'than the first parameter. For instance, (//* 19 5) is 15. Useful ' +
-      'for dividing data into equal-sized buckets (e.g. for histograms).'
+      'Produces the highest multiple of b less ' +
+      'than a. For instance, (//* 19 5) is 15. This can be ' +
+      'used to place the data in a into equal-sized buckets.'
     )
 };
 
@@ -190,7 +191,7 @@ var OpsMath = {
     });
   }).type('(fn (-> (name channel? "x")) (ref "x"))')
     .describe(
-      'Takes the square root of a number or channel.'
+      'Takes the square root of x.'
     ),
   pow: new FistFunction(function(args) {
     return _binaryOp(args.x, args.a, function(x, a) {
@@ -198,7 +199,8 @@ var OpsMath = {
     });
   }).type('(fn (-> (name channel? "x") (name number "a")) (ref "x"))')
     .describe(
-      'With two parameters (x, a), computes x^a.'
+      'With two parameters (x, a), produces x^a ' +
+      '(x to the power of a).'
     ),
   exp: new FistFunction(function(args) {
     if (args.a === undefined) {
@@ -211,8 +213,9 @@ var OpsMath = {
     });
   }).type('(fn (-> (name channel? "x") (name (? number) "a")) (ref "x"))')
     .describe(
-      'With one parameter x, computes e^x. With two parameters ' +
-      '(x, a), computes a^x.'
+      'If only x is provided, produces e^x (e to the power of x, ' +
+      'where e is roughly 2.718). If both (x, a) are provided, ' +
+      'produces a^x (a to the power of x).'
     ),
   log: new FistFunction(function(args) {
     if (args.b === undefined) {
@@ -225,8 +228,9 @@ var OpsMath = {
     });
   }).type('(fn (-> (name channel? "x") (name (? number) "b")) (ref "a"))')
     .describe(
-      'With one parameter x, computes ln x. With two parameters ' +
-      '(x, b), computes x log b.'
+      'If only x is provided, produces ln x (the logarithm of x base e, ' +
+      'where e is roughly 2.718). If both (x, b) are provided, ' +
+      'produces x log b (the logarithm of x base b).'
     ),
   floor: new FistFunction(function(args) {
     return _unaryOp(args.x, function(x) {
@@ -234,7 +238,7 @@ var OpsMath = {
     });
   }).type('(fn (name channel? "x") (ref "x"))')
     .describe(
-      'Rounds its parameter down.'
+      'Rounds x down to the nearest integer.'
     ),
   round: new FistFunction(function(args) {
     return _unaryOp(args.x, function(x) {
@@ -242,7 +246,7 @@ var OpsMath = {
     });
   }).type('(fn (name channel? "x") (ref "x"))')
     .describe(
-      'Rounds its parameter to the nearest integer.'
+      'Rounds x up or down to the nearest integer.'
     ),
   ceil: new FistFunction(function(args) {
     return _unaryOp(args.x, function(x) {
@@ -250,7 +254,7 @@ var OpsMath = {
     });
   }).type('(fn (name channel? "x") (ref "x"))')
     .describe(
-      'Rounds its parameter up.'
+      'Rounds x up to the nearest integer.'
     )
 };
 
@@ -285,7 +289,7 @@ var OpsTime = {
     };
   }).type('(fn (-> (name channel "c") (name timedelta "dt")) channel)')
     .describe(
-      'With two parameters (c, dt), time-shifts c by dt milliseconds. ' +
+      'Time-shifts c by dt milliseconds. ' +
       'For instance, (time-shift c 3600000) shifts c forward one hour, ' +
       'whereas (time-shift c "-1 minute") shifts c back one minute.'
     ),
@@ -344,9 +348,10 @@ var OpsTime = {
     return new DataChannel(_data);
   }).type('(fn (-> (name channel "c") (name string "reduce") (name timedelta "dt")) channel)')
     .describe(
-      'With three parameters (c, reduce, dt), groups the data points of c ' +
-      'into time buckets of width dt, then applies the given reducing ' +
-      'operation to each bucket.'
+      'Groups the data from c into time buckets of width dt, then ' +
+      'combines the data in each bucket according to reduce. ' +
+      'reduce can be one of "count", "sum", or "average", which ' +
+      'specifies the combining operation.'
     ),
   timeIdentity: new FistFunction(function(args) {
     return {
@@ -359,7 +364,7 @@ var OpsTime = {
     }
   }).type('(fn (name channel "c") channel)')
     .describe(
-      'Creates a new channel whose values are equal to its timestamps.'
+      'Produces a channel whose values are the timestamps of c.'
     ),
   hourOfDay: new FistFunction(function(args) {
     return {
@@ -372,8 +377,8 @@ var OpsTime = {
     }
   }).type('(fn (name channel "c") channel)')
     .describe(
-      'Creates a new channel whose values are the hours of day (0-24) ' +
-      'of its timestamps.'
+      'Produces a channel whose values are the hours of day (0-23) ' +
+      'corresponding to the timestamps of c.'
     ),
   dayOfWeek: new FistFunction(function(args) {
     return {
@@ -386,8 +391,8 @@ var OpsTime = {
     }
   }).type('(fn (name channel "c") channel)')
     .describe(
-      'Creates a new channel whose values are the days of week (0-6) ' +
-      'of its timestamps.'
+      'Produces a channel whose values are the days of week (0-6) ' +
+      'corresponding to the timestamps of c.'
     )
 };
 
@@ -423,8 +428,8 @@ var OpsSmooth = {
     return new DataChannel(_data);
   }).type('(fn (-> (name channel "c") (name timedelta "halfLife")) channel)')
     .describe(
-      'With two parameters (c, halfLife), applies a rolling average to c ' +
-      'that decays by 50% over the period given by halfLife.'
+      'Applies a rolling average to c ' +
+      'that decays by 50% over halfLife milliseconds.'
     ),
   slidingWindow: new FistFunction(function(args) {
     var _iter = args.c.iter(),
@@ -459,7 +464,7 @@ var OpsSmooth = {
     return new DataChannel(_data);
   }).type('(fn (-> (name channel "c") (name number "windowSize")) channel)')
     .describe(
-      'With two parameters (c, windowSize), applies a sliding window ' +
+      'Applies a sliding window ' +
       'average to c that uses the last windowSize data points.'
     )
 };
@@ -469,19 +474,18 @@ var OpsJoin = {
   join: new FistFunction(function(args) {
     return {
       at: function(t) {
-        return args.cs[0].at(t);
+        return args.c.at(t);
       },
       iter: function() {
-        return IntersectionIterator(
-          args.cs.map(function(c) { return c.iter(); })
-        );
+        var iters = args.joins.map(function(j) { return j.iter(); });
+        iters.unshift(args.c.iter());
+        return IntersectionIterator(iters);
       }
     }
-  }).type('(fn (name (+ channel) "cs") channel)')
+  }).type('(fn (-> (name channel "c") (name (+ channel) "joins")) channel)')
     .describe(
-      'With parameters (c1, ..., cN), creates a new channel with values ' +
-      'from c1 and only those timestamps present in every channel ' +
-      'c1, ..., cN.'
+      'Produces a channel with values from c, filtered to only those ' +
+      'timestamps present in both c and each of the joins channels.'
     )
 };
 
@@ -502,8 +506,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points less than its parameter.'
+      'Filters c to contain only values less than x.'
     ),
   lteq: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -511,8 +514,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points less than or equal to its parameter.'
+      'Filters c to contain only values less than or equal to x.'
     ),
   eq: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -521,8 +523,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points equal to its parameter.'
+      'Filters c to contain only values equal to x.'
     ),
   neq: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -531,8 +532,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points not equal to its parameter.'
+      'Filters c to contain only values not equal to x.'
     ),
   gteq: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -540,8 +540,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points greater than or equal to its parameter.'
+      'Filters c to contain only values greater than or equal to x.'
     ),
   gt: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -549,8 +548,7 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points greater than to its parameter.'
+      'Filters c to contain only values greater than x.'
     ),
   valueBetween: new FistFunction(function(args) {
     return _filterOp(args.c, function(t) {
@@ -559,15 +557,15 @@ var OpsFilterValue = {
     });
   }).type('(fn (-> (name channel "c") (name number "x1") (name number "x2")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points with values on [x1, x2).'
+      'Filters c to contain only values between x1 (inclusive) and ' +
+      'x2 (exclusive).'
     )
 };
 
 var OpsFilterTime = {
   __exports: [
-    ['until', 'time-until'],
     ['since', 'time-since'],
+    ['until', 'time-until'],
     ['between', 'time-between']
   ],
   __fullName: 'Time Filters',
@@ -578,8 +576,7 @@ var OpsFilterTime = {
     });
   }).type('(fn (-> (name channel "c") (name time "since")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'data points since the given time.'
+      'Filters c to contain only data points from on or after since.'
     ),
   until: new FistFunction(function(args) {
     var _until = Time.get(args.until);
@@ -588,8 +585,7 @@ var OpsFilterTime = {
     });
   }).type('(fn (-> (name channel "c") (name time "until")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'data points until the given time.'
+      'Filters c to contain only data points from before until.'
     ),
   between: new FistFunction(function(args) {
     var _since = Time.get(args.since),
@@ -599,8 +595,8 @@ var OpsFilterTime = {
     });
   }).type('(fn (-> (name channel "c") (name time "since") (name time "until")) channel)')
     .describe(
-      'Creates a filter that, when applied to a channel, selects only ' +
-      'those data points between the given times.'
+      'Filters c to contain only data points from between since ' +
+      '(inclusive) and until (exclusive).'
     )
 };
 
@@ -615,17 +611,17 @@ var GensData = {
     };
   }).type('(fn (name number "x") (fn number number))')
     .describe(
-      'Creates a data generator that, when evaluated at a timestamp, ' +
-      'returns its parameter.'
+      'Produces a data generator that, when evaluated at a timestamp, ' +
+      'returns x.'
     ),
   choice: new FistFunction(function(args) {
     return function(t) {
-      return Random.choice(args.xs);
+      return Random.choice(args.values);
     };
-  }).type('(fn (name (+ number) "xs") (fn number number))')
+  }).type('(fn (name (+ number) "values") (fn number number))')
     .describe(
-      'Creates a data generator that, when evaluated at a timestamp, ' +
-      'returns a value selected at random from its parameters.'
+      'Produces a data generator that, when evaluated at a timestamp, ' +
+      'returns a value selected at random from values.'
     ),
   uniform: new FistFunction(function(args) {
     return function(t) {
@@ -633,9 +629,8 @@ var GensData = {
     };
   }).type('(fn (-> (name number "min") (name number "max")) (fn number number))')
     .describe(
-      'With two parameters (min, max) creates a data generator that, when ' +
-      'evaluated at a timestamp, returns a uniform random value from ' +
-      'the interval [min, max).'
+      'Produces a data generator that, when evaluated at a timestamp, ' +
+      'returns a random value between min (inclusive) and max (exclusive).'
     ),
   gaussian: new FistFunction(function(args) {
     return function(t) {
@@ -643,9 +638,9 @@ var GensData = {
     };
   }).type('(fn (-> (name number "mu") (name number "sigma")) (fn number number))')
     .describe(
-      'With two parameters (mu, sigma) creates a data generator that, when ' +
-      'evaluated at a timestamp, returns a random value from the Gaussian ' +
-      'distribution G(mu, sigma^2).'
+      'Produces a data generator that, when evaluated at a timestamp, ' +
+      'returns a normally distributed value with mean mu and ' +
+      'standard deviation sigma.'
     ),
 };
 var GensChannel = {
@@ -666,9 +661,8 @@ var GensChannel = {
     return new DataChannel(_data);
   }).type('(fn (-> (name (fn number number) "gen") (name number "since") (name number "until") (name number "n")) channel)')
     .describe(
-      'With three parameters (since, until, n) creates a channel generator ' +
-      'that, when applied to a data generator, builds a channel having n ' +
-      'evenly spaced data points with timestamps on [since, until).'
+      'Produces a channel by evaluating gen at n evenly spaced timestamps ' +
+      'between since (inclusive) and until (exclusive).'
     ),
   genUniform: new FistFunction(function(args) {
     var _dts = Random.combination(args.until - args.since, args.n),
@@ -680,9 +674,8 @@ var GensChannel = {
     return new DataChannel(_data);
   }).type('(fn (-> (name (fn number number) "gen") (name number "since") (name number "until") (name number "n")) channel)')
     .describe(
-      'With three parameters (since, until, n) creates a channel generator ' +
-      'that, when applied to a data generator, builds a channel having n ' +
-      'data points with timestamps randomly selected from [since, until).'
+      'Produces a channel by evaluating gen at n timestamps selected ' +
+      'randomly from between since (inclusive) and until (exclusive).'
     ),
   genPoisson: new FistFunction(function(args) {
     var _t = args.since,
@@ -696,10 +689,11 @@ var GensChannel = {
     return new DataChannel(_data);
   }).type('(fn (-> (name (fn number number) "gen") (name number "since") (name number "until") (name number "rate")) channel)')
     .describe(
-      'With three parameters (since, until, rate) creates a channel generator ' +
-      'that, when applied to a data generator, builds a channel having ' +
-      'roughly one data point every rate milliseconds with timestamp ' +
-      'on [since, until).'
+      'Produces a channel by evaluating gen roughly once every rate ' +
+      'milliseconds between since (inclusive) and until (exclusive). ' +
+      'The exact timestamps are computed using a Poisson distribution, ' +
+      'which models events happening randomly over time (e.g. customers ' +
+      'arriving at a store, emails sent to your inbox).'
     )
 };
 
